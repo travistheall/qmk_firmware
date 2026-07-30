e4babb73a1670fe7164379c1ce9758cc757a3f20
Travis Theall <travistheall@outlook.com>
2026-07-30 12:15:32 -0500

    update keymaps

--- keyboards/keebio/quefrency/keymaps/tnt/keymap.c
+++ keyboards/keebio/quefrency/keymaps/tnt/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,     KC_TAB,    KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,         KC_U,         KC_I,         KC_O,          KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
     KC_F3,   KC_F4,     KC_ESCAPE, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,         KC_J,         KC_K,         KC_L,          KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
     KC_F5,   KC_F6,     KC_LSFT,   KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,         KC_M,         KC_COMM,      KC_DOT,        KC_SLSH, KC_RSFT, KC_UP,   KC_END,
-    KC_F7,   KC_F8,     KC_LCTL,   KC_LALT, KC_LGUI, MO(1),    LT(1,KC_SPC),                   LT(1,KC_SPC), LT(1,KC_SPC), LT(1,KC_SPC),  KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F7,   KC_F8,     KC_LALT,   KC_LCTL, KC_LGUI, MO(1),    LT(1,KC_SPC),                   LT(1,KC_SPC), LT(1,KC_SPC), KC_RCTL,  KC_RALT, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [1] = LAYOUT_65_with_macro(
--- keyboards/keebio/quefrency/keymaps/tnt/rules.mk
+++
@@ -1,3 +0,0 @@
-# https://caniusevia.com/docs/configuring_qmk
-VIA_ENABLE = yes
-ENCODER_ENABLE = yes
